d59867ff789edda37b27549c460eabfe1c2b40e9
Rejoan Islam Afridi <47922447+rejoanafridi@users.noreply.github.com>
2021-08-01 12:49:31 +0600

    initial commit

--- Database sql/ledp_evening.sql
+++ Database sql/ledp_evening.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2021 at 05:11 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Aug 01, 2021 at 08:48 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,17 @@ SET time_zone = "+00:00";
 --
 -- Database: `ledp_evening`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `afridi`
+--
+
+CREATE TABLE `afridi` (
+  `name` varchar(100) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -40,11 +51,8 @@ CREATE TABLE `ledp_registration` (
 -- Dumping data for table `ledp_registration`
 --
 
-INSERT INTO `ledp_registration` (`Id`, `Name`, `mother`, `Course`, `email`, `district`) VALUES
-(11, 'Md Maruf Hossain', 'Rijia Begum', 'ict', 'marufhossain5454@gmail.com', 'Jessore'),
-(13, 'fahad', 'Asia Khatun', 'ICT', 'fahad1122@gmail.com', 'lashampur'),
-(14, 'hlw', 'Asia Khatun', 'cseeeee', 'fffff@gmail.com', 'Jessore'),
-(15, 'Naim', 'mmmmmmm', 'cseeeee', 'naim@gmail.com', 'jashore');
+INSERT INTO `ledp_registration` (`Id`, `Name`, `teacher`, `Course`, `email`, `section`) VALUES
+(16, 'afridi', '', 'Web Engineering', 'rejoan15-11431@diu.edu.bd', '');
 
 -- --------------------------------------------------------
 
@@ -71,18 +79,8 @@ INSERT INTO `ledp_signup` (`Id`, `Name`, `email`, `Password`) VALUES
 (5, '', 'hakim@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
 (6, '', 'fahad@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
 (7, '', 'marufcoed@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
-(8, '', 'Bisso@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `maruf`
---
-
-CREATE TABLE `maruf` (
-  `name` varchar(100) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(8, '', 'Bisso@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(9, '', 'rejoan15-11431@diu.edu.bd', '827ccb0eea8a706c4c34a16891f84e7b');
 
 --
 -- Indexes for dumped tables
@@ -108,13 +106,13 @@ ALTER TABLE `ledp_signup`
 -- AUTO_INCREMENT for table `ledp_registration`
 --
 ALTER TABLE `ledp_registration`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `ledp_signup`
 --
 ALTER TABLE `ledp_signup`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
